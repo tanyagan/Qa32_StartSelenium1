28cef6b25f373eedb7b6f4e783cb9d94535a95ce
tanyagan <avgustan2012@gmail.com>
2022-03-13 02:26:55 +0200

    Homework2

--- src/test/java/StartPhoneBook.java
+++ src/test/java/StartPhoneBook.java
@@ -6,132 +6,117 @@ import org.testng.annotations.Test;
 
 import java.util.List;
 
-
 public class StartPhoneBook {
 
     WebDriver wd;
 
     @Test
-    public void startPhoneBook() {
+    public void startPhoneBook(){
         wd = new ChromeDriver();
 
         //System.setProperty("webdriver.chrome.driver", "path");
-        // wd.get("https://contacts-app.tobbymarshall815.vercel.app/login"); //open
 
-        wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/");  //open www+ history
+        // wd.get("https://contacts-app.tobbymarshall815.vercel.app/login");// open
+        wd.navigate().to(" https://contacts-app.tobbymarshall815.vercel.app/");// open www + history
         wd.manage().window().maximize();
 
-        // steps of test login Succes:
-        // open login form ....find loginbtn + click
 
-        //WebElement el = wd.findElement(By.id("idel"));
+        // steps of test login Success:
+        // open login form  --- find loginbtn + click  --> opened form login
+
+
+
+        // WebElement el = wd.findElement(By.id("idel"));
         // List<WebElement> list = wd.findElements(By.id("idel"));
-        //WebElement a  = wd.findElement(By.tagName("a"));---->Home
-        List<WebElement> elements = wd.findElements(By.tagName("a"));
+        //WebElement a = wd.findElement(By.tagName("a")); --- > HOME
+        List <WebElement> elements = wd.findElements(By.tagName("a"));
         WebElement login = elements.get(2);
         login.click();
 
-
+        WebElement element = wd.findElement(By.id("root"));
         WebElement element1 = wd.findElement(By.className("container"));
-        // fill email.....,find email element + click + clear + type email
-        //fill password....fine password element + click + clear + type password
-        //click login  btn...find login btn element + click
+
+        // fill email   --- find email element+ click+clear+type email
+        //fill password ---  find password element+ click+clear+type password
+        //click login btn  find login btn element+ click
 
 
         // Assert
 
+        // wd.close(); // close one tab
+        // wd.quit(); // close all tabs
 
-        // wd.close();
-        //wd.quit();
     }
 
+
     @Test
-    public void home() {
+    public void home(){
         wd = new ChromeDriver();
-        wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/Login");
+        wd.navigate().to(" https://contacts-app.tobbymarshall815.vercel.app/login");
         wd.manage().window().maximize();
 
-        WebElement el = wd.findElement(By.tagName("head"));
-        WebElement el1 = wd.findElement(By.tagName("meta"));
-        WebElement el2 = wd.findElement(By.tagName("link"));
-        WebElement el3 = wd.findElement(By.tagName("body"));
-        WebElement el4 = wd.findElement(By.tagName("div"));
-        WebElement el5 = wd.findElement(By.tagName("h1"));
-        WebElement el6 = wd.findElement(By.tagName("input"));
-        WebElement el7 = wd.findElement(By.tagName("button"));
-        WebElement el8 = wd.findElement(By.tagName("a"));
+        // wd.findElement(By.tagName());
 
-        WebElement el9 = wd.findElement(By.id("root"));
+        WebElement el1 = wd.findElement(By.tagName("body"));
+        WebElement el2 = wd.findElement(By.tagName("div"));
+        WebElement el3 = wd.findElement(By.tagName("h1"));
+        WebElement el7 = wd.findElement(By.tagName("a"));
+        WebElement el4 = wd.findElement(By.tagName("input"));
+        WebElement el5 = wd.findElement(By.tagName("br"));
+        WebElement el6 = wd.findElement(By.tagName("button"));
 
-        WebElement el10 = wd.findElement(By.className("container"));
-        WebElement el11 = wd.findElement(By.className("navbar-component_nav__1X_4m"));
-        WebElement el12 = wd.findElement(By.className("active"));
-        WebElement el13 = wd.findElement(By.className("login_login__3EHKB"));
 
-        WebElement el 14 = wd.findElement(By.linkText("Home"));
-        WebElement el 14 = wd.findElement(By.linkText("ABOUT"));
-        WebElement el 14 = wd.findElement(By.linkText("LOGIN"));
-        WebElement el 14 = wd.findElement(By.linkText("HO"));
+        // wd.findElement(By.id());
+        WebElement el8 = wd.findElement(By.id("root"));
+        // By.className
+        WebElement el9 = wd.findElement(By.className("container"));
+        WebElement el10 = wd.findElement(By.className("navbar-component_nav__1X_4m"));
+        WebElement el11 = wd.findElement(By.className("active"));
+        WebElement el12 = wd.findElement(By.className("login_login__3EHKB"));
+
+
+        // byLinkText /// HOME,ABOUT,LOGIN,FVMLFBM,EEE,22R,DFG,GHH,FGHH,THH
+        WebElement el13 = wd.findElement(By.linkText("HOME"));
+        WebElement el14 = wd.findElement(By.linkText("ABOUT"));
+        WebElement el15 = wd.findElement(By.linkText("LOGIN"));
+        WebElement el16 = wd.findElement(By.partialLinkText("HO"));
+
     }
 
+
     @Test
-    public void cssLocatorsTest() {
+    public void cssLocatorsTest(){
         wd = new ChromeDriver();
-        wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/Login");
+        wd.navigate().to(" https://contacts-app.tobbymarshall815.vercel.app/login");
         wd.manage().window().maximize();
 
-        wd.findElement(By.cssSelector(""));
-        //by tag name
+        //wd.findElement(By.cssSelector(""));
+        // by tag name
         wd.findElement(By.cssSelector("div"));
+        wd.findElement(By.xpath("//div"));
         wd.findElement(By.cssSelector("a"));
-        //by class
+        wd.findElement(By.xpath("//a")); //   /html/body/div/div/a
+
+        // by class
         wd.findElement(By.cssSelector(".login_login__3EHKB"));
+        wd.findElement(By.xpath("//*[@class='login_login__3EHKB']"));
         // by id
         wd.findElement(By.cssSelector("#root"));
-        //отрибут
+        wd.findElement(By.xpath("//*[@id='root']"));
+        // атрибут
         wd.findElement(By.cssSelector("[placeholder]"));
+        wd.findElement(By.xpath("//*[@placeholder]"));
+
         wd.findElement(By.cssSelector("[placeholder='Password']"));
+        wd.findElement(By.xpath("//*[@placeholder ='Password']"));
+
         wd.findElement(By.cssSelector("[placeholder *='ss']"));
+        wd.findElement(By.xpath("//*[contains(@placeholder,'ss')]"));
+
         wd.findElement(By.cssSelector("[placeholder ^='Pa']"));
+        wd.findElement(By.xpath("//*[starts-with(@placeholder,'Pa')]"));
+
         wd.findElement(By.cssSelector("[placeholder $='rd']"));
-
-
-        wd.findElement(By.cssSelector("#customers tr:nth-child(2) td:nth-child(2)"));
-        wd.findElement(By.xpath("//*[@id='customers']//tr[2]/td[2]"));
-
-        @Test
-        public void cssLocatorsTest(){
-            wd = new ChromeDriver();
-            wd.navigate().to(" https://contacts-app.tobbymarshall815.vercel.app/login");
-            wd.manage().window().maximize();
-
-            wd.findElement(By.cssSelector(""));
-            // by tag name
-            wd.findElement(By.cssSelector("div"));
-            wd.findElement(By.xpath("//div"));
-            wd.findElement(By.cssSelector("a"));
-            wd.findElement(By.xpath("//a")); //   /html/body/div/div/a
-
-            // by class
-            wd.findElement(By.cssSelector(".login_login__3EHKB"));
-            wd.findElement(By.xpath("//*[@class='login_login__3EHKB']"));
-            // by id
-            wd.findElement(By.cssSelector("#root"));
-            wd.findElement(By.xpath("//*[@id='root']"));
-            // атрибут
-            wd.findElement(By.cssSelector("[placeholder]"));
-            wd.findElement(By.xpath("//*[@placeholder]"));
-
-            wd.findElement(By.cssSelector("[placeholder='Password']"));
-            wd.findElement(By.xpath("//*[@placeholder ='Password']"));
-
-            wd.findElement(By.cssSelector("[placeholder *='ss']"));
-            wd.findElement(By.xpath("//*[contains(@placeholder,'ss')]"));
-
-            wd.findElement(By.cssSelector("[placeholder ^='Pa']"));
-            wd.findElement(By.xpath("//*[starts-with(@placeholder,'Pa')]"));
-
-            wd.findElement(By.cssSelector("[placeholder $='rd']"));
 
 
 
--- src/test/java/TableTest.java
+++ src/test/java/TableTest.java
@@ -1,5 +1,8 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TableTest {
@@ -11,47 +14,65 @@ public class TableTest {
             wd.navigate().to("https://www.w3schools.com/css/css_table.asp");
             wd.manage().window().maximize();
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
-            public class TableTest {
-
-                WebDriver wd;
-
-                @Test
-                public void tableCss(){
-                    wd = new ChromeDriver();
-                    wd.manage().window().maximize();
-                    wd.navigate().to("https://www.w3schools.com/css/css_table.asp");
-
-                    WebElement el = wd.findElement(By.cssSelector("#customers td:last-child"));
+            WebElement el = wd.findElement(By.cssSelector("#customers td:last-child"));
                 }
 
                 @Test
-                public void homework(){
+                public void homework() {
                     wd = new ChromeDriver();
                     wd.manage().window().maximize();
                     wd.navigate().to("file:///C:/Users/Tatiana/Downloads/index%20(1).html");
 
                     WebElement item1 = wd.findElement(By.cssSelector("[href='#item1']"));
                     WebElement item1_2 = wd.findElement(By.cssSelector("#nav ul li:first-child a"));
+                    WebElement item2 = wd.findElement(By.cssSelector("#nav ul li:nth-child(2)"));
+                    WebElement item2_2 = wd.findElement(By.cssSelector("[href='#item2']"));
+                    WebElement item3 = wd.findElement(By.cssSelector("[href='#item3']"));
+                    WebElement item3_2 = wd.findElement(By.cssSelector("#nav ul li:nth-child(3)"));
+                    WebElement item4 = wd.findElement(By.cssSelector("#nav ul li:last-child"));
+                    System.out.println(item4.getText());
+                    WebElement item4_2 = wd.findElement(By.cssSelector("[href='#item4']"));
+
 
                     WebElement inputName = wd.findElement(By.cssSelector("[placeholder='Type your name']"));
                     WebElement inputName2 = wd.findElement(By.cssSelector("[name='name']"));
+                    WebElement inputName3 = wd.findElement(By.cssSelector("#form1 [name='name']"));
+                    WebElement inputName4 = wd.findElement(By.cssSelector("[placeholder='Type your surename']"));
+                    WebElement inputName5 = wd.findElement(By.cssSelector("[name='surename']"));
+                    WebElement inputName6 = wd.findElement(By.cssSelector("#form1 [name='surename']"));
+
+
+                    //WebElement but = wd.findElement( By.ByCssSelector(".btn"));?
+
+                    WebElement red = wd.findElement(By.cssSelector("#alert .message"));
+
+                    WebElement table = wd.findElement(By.cssSelector("#country-table td:first-child"));
+                    WebElement table1 = wd.findElement(By.cssSelector("#country-table td:nth-child(2)"));
+                    WebElement table2 = wd.findElement(By.cssSelector("#country-table td:last-child"));
+                    WebElement table3 = wd.findElement(By.cssSelector("tr:nth-child(1) td:nth-child(1)"));
+                    WebElement table4 = wd.findElement(By.cssSelector("tr:nth-child(2) td:nth-child(1)"));
+                    WebElement table5 = wd.findElement(By.cssSelector("tr:nth-child(3) td:nth-child(1)"));
+                    WebElement table6 = wd.findElement(By.cssSelector("tr:nth-child(4) td:nth-child(1)"));
+                    WebElement table7 = wd.findElement(By.cssSelector("tr:nth-child(1) td:nth-child(2)"));
+                    WebElement table8 = wd.findElement(By.cssSelector("tr:nth-child(2) td:nth-child(2)"));
+                    WebElement table9 = wd.findElement(By.cssSelector("tr:nth-child(3) td:nth-child(2)"));
+                    WebElement table10 = wd.findElement(By.cssSelector("tr:nth-child(4) td:nth-child(2)"));
+                    WebElement table11 = wd.findElement(By.cssSelector("tr:nth-child(1) td:nth-child(3)"));
+                    WebElement table12 = wd.findElement(By.cssSelector("tr:nth-child(2) td:nth-child(3)"));
+                    WebElement table13 = wd.findElement(By.cssSelector("tr:nth-child(3) td:nth-child(3)"));
+                    WebElement table14= wd.findElement(By.cssSelector("tr:nth-child(4) td:nth-child(3)"));
+
 
                     // Russia
-                    WebElement el =wd.findElement(By.cssSelector("tr:nth-child(3) td:nth-child(2)"));
+                    WebElement el = wd.findElement(By.cssSelector("tr:nth-child(3) td:nth-child(2)"));
 
 
-                    WebElement item4 = wd.findElement(By.cssSelector("#nav ul li:last-child"));
-                    System.out.println(item4.getText());
+                }
 
-                    //  WebElement but = wd.findElement(By.ByCssSelector("[.'btn']"));
+                @Test
+                public void tableCss1(){
                     //WebElement but = wd.findElement(By.ByCssSelector("[.'btn']"));
+
 
                     wd.findElement(By.xpath("//*[text()='Maria Anders']"));
                     wd.findElement(By.cssSelector("#customers tr:nth-child(2) td:nth-child(2)"));
@@ -63,7 +84,7 @@ import org.testng.annotations.Test;
                 public void xPathTests(){
                     wd = new ChromeDriver();
                     wd.manage().window().maximize();
-                    wd.navigate().to("file:///Users/tayahatum/Downloads/apk/index.html");
+                    wd.navigate().to("file:///C:/Users/Tatiana/Downloads/index%20(1).html");
 
                     WebElement item1 = wd.findElement(By.xpath("//a[@href='#item1']"));
                     String textItem1 = item1.getText();
@@ -96,12 +117,3 @@ import org.testng.annotations.Test;
             }
 
 
-        }
-
-
-    }
-
-
-
-
-}
